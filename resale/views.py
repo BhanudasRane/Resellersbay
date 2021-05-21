d973e85051b89a2c188bfa42b9ec95aef214b102
BhanudasRane1 <ranebhanudas786@gmail.com>
2021-05-21 12:59:11 +0000

    urls mapping for index

--- resale/views.py
+++ resale/views.py
@@ -4,3 +4,27 @@ from django.conf import settings
 
 def index(request):
     return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def contact(request):
+    return render(request,'contact.html')
+def detailproduct(request):
+    return render(request,'detail-product.html')
+
+
+def faq(request):
+    return render(request,'faq.html')
+def privacy(request):
+    return render(request,'privacy.html')
+def setting(request):
+    return render(request,'setting.html')
+def shop(request):
+    return render(request,'shop.html')
+def terms(request):
+    return render(request,'terms.html')
+def transaction(request):
+    return render(request,'transaction.html')
